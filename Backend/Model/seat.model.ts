db7f1f94493e7a2cd8d58e3d57f10cfe99da6455
Sejal Jaiswal <710sejal@gmail.com>
2023-06-11 10:25:00 +0530

    main

--- Backend/Model/seat.model.ts
+++ Backend/Model/seat.model.ts
@@ -3,19 +3,20 @@
 import mongoose, { Document, Model, Schema } from "mongoose";
 
 export interface ISeat extends Document {
-  seatNumber: string;
+  seatNumber: number;
   isBooked: boolean;
+  name : string
 }
 
 export const seatSchema: Schema<ISeat> = new mongoose.Schema(
   {
     seatNumber: {
-      type: String,
+      type: Number,
       required: true,
       unique: true,
       validate: {
-        validator: function (value: string) {
-          return parseInt(value) >= 1 && parseInt(value) <= 80;
+        validator: function (value: number) {
+          return (value >= 1 && value <= 80);
         },
         message: "Seat number must be between 1 and 80",
       },
@@ -24,6 +25,9 @@ export const seatSchema: Schema<ISeat> = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    name:{
+      type:String
+    }
   },
   {
     versionKey: false,
@@ -34,7 +38,7 @@ seatSchema.statics.createSeats = async function (this: Model<ISeat>) {
   try {
     const seats = [];
     for (let i = 1; i <= 80; i++) {
-      seats.push({ seatNumber: i.toString(), isBooked: false });
+      seats.push({ seatNumber: i, isBooked: false });
     }
     await this.insertMany(seats);
     console.log("Seats created successfully.");
--- Backend/index.ts
+++ Backend/index.ts
@@ -49,7 +49,7 @@ app.get("/seats",async(req: Request,res: Response) => {
 
 app.patch("/seats/reset", async (req: Request, res: Response) => {
   try {
-    await SeatModel.updateMany({}, { $set: { isBooked: false } });
+    await SeatModel.updateMany({}, { $set: { isBooked: false,name:"" } });
     return res.json({ message: "All seats reset to false." });
   } catch (error: any) {
     console.error("Error resetting seats:", error);
@@ -57,42 +57,110 @@ app.patch("/seats/reset", async (req: Request, res: Response) => {
   }
 });
 
+
+// app.patch('/seats', async (req: Request, res: Response) => {
+//   const { seats, name } = req.body;
+
+//   try {
+//     // const availableSeats = await SeatModel.find({ isBooked: false }).sort("seatNumber").lean();
+//     const availableSeats = await SeatModel.aggregate([
+//       { $match: { isBooked: false } },
+//       { $addFields: { seatNumber: { $toInt: "$seatNumber" } } },
+//       { $sort: { seatNumber: 1 } }
+//     ]);
+//     let k = 1,index=0;
+//     let bookedSeats : number[] = []
+//     for(let i=1;i<=12;i++){
+//       let count = 0;
+//       let length = i == 12 ? 3 : 7;
+//       bookedSeats = [];
+//       for(let j=1;j<=length;j++){
+        
+//         if(availableSeats[index] !== undefined && availableSeats[index].seatNumber == k && count <= seats){
+//           bookedSeats.push(availableSeats[index]._id)
+//           count++;
+//         }
+//         k++;
+//         index++;
+//       }
+//       if(count >= seats){
+//           break;
+//       }
+//       }
+//       console.log(count);
+      
+//     }
+//     res.send(bookedSeat)
+//   } 
+//   catch (error: any) {
+//     console.error('Error booking seats:', error);
+//     return res.status(500).json({ error: 'An error occurred while booking the seats.' });
+//   }
+// });
+
+
 app.patch('/seats', async (req: Request, res: Response) => {
-  const seatNumbers = req.body.seats;
+  const { seats, name } = req.body;
 
   try {
-    const bookedSeats: string[] = [];
+    const availableSeats = await SeatModel.aggregate([
+      { $match: { isBooked: false } },
+      { $addFields: { seatNumber: { $toInt: "$seatNumber" } } },
+      { $sort: { seatNumber: 1 } }
+    ]);
 
-    for (const seatNumber of seatNumbers) {
-      
-      const seat = await SeatModel.findOne({ seatNumber });
-
-     
-      if (!seat) {
-        return res.status(404).json({ error: `Seat ${seatNumber} not found.` });
+    let k = 1, index = 0;
+    let bookedSeats: any[] = [];
+    let seatsBook: number[] = []
+    for (let i = 1; i <= 12; i++) {
+      let count = 1;
+      let length = i === 12 ? 3 : 7;
+      bookedSeats = [];
+      seatsBook = []
+      for (let j = 1; j <= length; j++) {
+        
+        if (availableSeats[index] !== undefined && availableSeats[index].seatNumber === k && count <= seats) {
+          bookedSeats.push(availableSeats[index]._id);
+          seatsBook.push(availableSeats[index].seatNumber);
+          index++;
+          count++;
+        }
+        if(count > seats){
+          break;
+        }
+        k++;
       }
-
-      
-      if (seat.isBooked) {
-        return res.status(400).json({ error: `Seat ${seatNumber} is already booked.` });
+      console.log(count,"count")
+      if (count >= seats) {
+        break;
       }
-
-      
-      seat.isBooked = true;
-      await seat.save();
-
-      
-      bookedSeats.push(seatNumber);
     }
 
-    return res.json({ message: `Seats ${bookedSeats.join(', ')} have been booked successfully.` });
+    if(bookedSeats.length === 0){
+      for(let i=1;i<=80;i++){
+        if(availableSeats[i] !== undefined){
+          bookedSeats.push(availableSeats[i]._id);
+          seatsBook.push(availableSeats[i].seatNumber)
+        }
+      }
+      if(bookedSeats.length === 0){
+        res.send({SeatNumber :"Not Available"});
+        return ;
+      }
+    }
+    // Update the documents
+    const result = await SeatModel.updateMany(
+      { _id: { $in: bookedSeats } },
+      { $set: { isBooked: true, name } }
+    );
+    console.log(result)
+
+    res.json({ SeatNumber: seatsBook});
   } catch (error: any) {
     console.error('Error booking seats:', error);
     return res.status(500).json({ error: 'An error occurred while booking the seats.' });
   }
 });
-
-
 
 
 app.listen(process.env.PORT, async(): Promise<void> => {
